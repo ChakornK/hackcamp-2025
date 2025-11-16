0986d5a4a0a0ed0529a9a9298abd2bcbc42ada56
Ryan Huang <ryanhuang6391@gmail.com>
2025-11-15 22:10:16 -0800

    feat(dbd):added multiple leaderboards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,29 @@
-import Leaderboard from "./components/leaderboard";
+import Leaderboard from "./components/Leaderboard";
 
 export default function Home() {
+  const leaderboardData = [
+    {
+      title: "Leaderboard",
+      data: [
+        { rank: 1, name: "Donald Trump", value: "3hr 5min" },
+        { rank: 2, name: "Agartha", value: "2hr 59min" },
+        { rank: 3, name: "Yo mama", value: "1hr 9min" },
+      ],
+    },
+    {
+      title: "Hall of shame :(",
+      data: [
+        { rank: 1, name: "Donald Trump", value: "35%" },
+        { rank: 2, name: "Big Beautiful Bill", value: "42%" },
+        { rank: 3, name: "Diddy Blud", value: "67%" },
+      ],
+    },
+  ];
+
   return (
     <main className="bg-[#E5E7EB] p-8 min-h-screen">
       <p>Hello skibidi sigmas!!</p>
-      <Leaderboard />
+      <Leaderboard leaderboards={leaderboardData} />
     </main>
   );
 }
