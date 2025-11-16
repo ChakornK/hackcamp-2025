8bfee37b688a6b6931de8ea708db58a184bbf095
ChakornK <chakornk2007@gmail.com>
2025-11-15 22:08:32 -0800

    feat(chrome ext): add start/stop functionality

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -6,9 +6,13 @@ let progressSeconds = 0;
 const totalDuration = 25 * 60 * 1000; // 25 minutes
 const updateInterval = 1000; // Update every 1s
 
+let startTimestamp = 0;
+let timeOffset = 0;
+
+let timerInterval;
 function updateTimer() {
-  progress += (updateInterval / totalDuration) * 100;
-  progressSeconds += updateInterval / 1000;
+  progressSeconds = Math.floor((Date.now() - startTimestamp + timeOffset) / 1000);
+  progress = ((Date.now() - startTimestamp + timeOffset) / totalDuration) * 100;
   if (progress > 100) progress = 100;
 
   timerCircle.style.strokeDasharray = `${progress} ${100 - progress}`;
@@ -19,12 +23,38 @@ function updateTimer() {
     .padStart(2, "0")}:${((progressSeconds % 3600) % 60).toString().padStart(2, "0")}`;
 
   if (progress < 100) {
-    setTimeout(updateTimer, updateInterval);
+    timerInterval = setTimeout(updateTimer, updateInterval);
   }
 }
 
 // Initialize strokeDasharray
 timerCircle.style.strokeDasharray = `0 100`;
 
-// Start the timer
-updateTimer();
+const startButton = document.getElementById("start-button");
+const stopButton = document.getElementById("stop-button");
+
+let running = false;
+startButton.addEventListener("click", () => {
+  clearTimeout(timerInterval);
+  if (running) {
+    startButton.innerText = "Start";
+    timeOffset += Date.now() - startTimestamp;
+    running = false;
+    return;
+  }
+  startTimestamp = Date.now();
+  startButton.innerText = "Pause";
+  running = true;
+  updateTimer();
+});
+
+stopButton.addEventListener("click", () => {
+  clearTimeout(timerInterval);
+  progress = 0;
+  progressSeconds = 0;
+  startTimestamp = 0;
+  timeOffset = 0;
+  startButton.innerText = "Start";
+  timerCircle.style.strokeDasharray = `0 100`;
+  timerText.textContent = "00:00:00";
+});
