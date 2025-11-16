3e0bc853186a00bb6d0090ad0499290f991444ce
ChakornK <chakornk2007@gmail.com>
2025-11-15 22:51:10 -0800

    fix(chrome ext): add stop confirmation modal

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -33,6 +33,38 @@ timerCircle.style.strokeDasharray = `0 100`;
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 
+const cancelStopButton = document.getElementById("cancel-stop-button");
+const confirmStopButton = document.getElementById("confirm-stop-button");
+const modalBg = document.querySelector(".modal-bg");
+const modalBox = document.querySelector(".modal-box");
+
+const showModal = () => {
+  modalBg.style.opacity = "1";
+  modalBg.style.pointerEvents = "auto";
+  modalBox.style.transform = "scale(1)";
+};
+const hideModal = () => {
+  modalBg.style.opacity = "0";
+  modalBg.style.pointerEvents = "none";
+  modalBox.style.transform = "scale(0.8)";
+};
+
+cancelStopButton.addEventListener("click", () => {
+  hideModal();
+});
+confirmStopButton.addEventListener("click", () => {
+  hideModal();
+  clearTimeout(timerInterval);
+  progress = 0;
+  progressSeconds = 0;
+  startTimestamp = 0;
+  timeOffset = 0;
+  running = false;
+  startButton.innerText = "Start";
+  timerCircle.style.strokeDasharray = `0 100`;
+  timerText.textContent = "00:00:00";
+});
+
 let running = false;
 startButton.addEventListener("click", () => {
   clearTimeout(timerInterval);
@@ -49,13 +81,5 @@ startButton.addEventListener("click", () => {
 });
 
 stopButton.addEventListener("click", () => {
-  clearTimeout(timerInterval);
-  progress = 0;
-  progressSeconds = 0;
-  startTimestamp = 0;
-  timeOffset = 0;
-  running = false;
-  startButton.innerText = "Start";
-  timerCircle.style.strokeDasharray = `0 100`;
-  timerText.textContent = "00:00:00";
+  showModal();
 });
