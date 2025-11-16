8165cb156db14203976bb5334f869babb75895bc
ChakornK <chakornk2007@gmail.com>
2025-11-15 20:37:44 -0800

    feat: add font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,11 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
